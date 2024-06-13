fbf2fa5a47c28b48eb863c44296e820e5419a8f6
unbeauvoyage <lavieestunbonvoyage@hotmail.com>
2024-06-13 16:07:06 +0900

    add plugin-http

--- src/components/ReactCard.tsx
+++ src/components/ReactCard.tsx
@@ -1,13 +1,30 @@
 import {useState} from "react";
 
+import {fetch} from "@tauri-apps/plugin-http"
+
 interface ReactCardProps {
     text?: string
 }
 export default function ReactCard({text}: ReactCardProps) {
     const [content, setText] = useState<string>("")
+
+
+    const [jsonData, setJsonData] = useState()
+    const fetchJson = async ()=>{
+        let data
+        try {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+            data = await res.json()
+        } catch(e) {
+            console.log('errrrrr', e)
+        }
+        setJsonData(data)
+    }
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <button onClick={()=>{setText("new content")}}>click</button>
+            <button onClick={fetchJson}>fetchJson</button>
+            <h1>{jsonData}</h1>
             <h1 className="text-6xl">React Card</h1>
             <p className="text-xl">This is a React card component</p>
             <p>{content}</p>
