b853e6d91c1d639110fdd979951b5e52012e79d5
unbeauvoyage <lavieestunbonvoyage@hotmail.com>
2024-06-14 01:27:40 +0900

    install tauri-plugin-clipboard-manager

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,8 +1,9 @@
 import {useStore} from "@nanostores/react";
 import {htmlContents} from "../HtmlContentStore.ts";
-
+import { readText } from '@tauri-apps/plugin-clipboard-manager';
 import {fetch} from "@tauri-apps/plugin-http"
 import ReactCard from "./ReactCard.tsx";
+import {useState} from "react";
 
 interface ReactCardProps {
     text?: string
@@ -33,11 +34,22 @@ export default function CardList() {
         htmlContents.set($htmlContents.slice(0, -1))
     }
 
-
+    const [clipboardContent, setClipboardContent] = useState<string>('');
+    const readClipboard = async () => {
+        const content = await readText();
+        setClipboardContent(content);
+        console.log(content);
+        // const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        // const data = await response.json()
+        // console.log(data)
+    }
     return (
         <>
             <button className={"text-white bg-gray-900 border-2"} onClick={addHtmlContent}>add html content</button>
             <button className={"text-white bg-gray-900 border-2"} onClick={deleteHtmlContent}>delete html content</button>
+            <button className={"text-white bg-gray-900 border-2"} onClick={readClipboard}> read clipboard </button>
+            <p>{clipboardContent}</p>
+            {/* <button onClick={fetchJson}>fetch json</button> */}
             <div className="flex flex-col h-screen">
                 {
                     $htmlContents.map((content) => <ReactCard content={content}/>)
